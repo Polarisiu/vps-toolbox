84e72a4130f8aac5e64fcd1cf814b8c9ff66f0ec
Polaris <fdyrwaas@outlook.com>
2025-10-05 11:18:31 +0800

    Update uu.sh

--- uu.sh
+++ uu.sh
@@ -21,8 +21,8 @@ if [ ! -f "$SCRIPT_PATH" ]; then
     chmod +x "$SCRIPT_PATH"
     ln -sf "$SCRIPT_PATH" "$BIN_LINK_DIR/m"
     ln -sf "$SCRIPT_PATH" "$BIN_LINK_DIR/M"
-    echo -e "${GREEN}✅ 安装完成${RESET}"
-    echo -e " ${GREEN}👉你可以输入 ${RED}m${RESET}${GREEN} 或 ${RED}M${RESET}${GREEN} 运行 UU 工具箱${RESET}"
+    echo -e "${GREEN}✅安装完成${RESET}"
+    echo -e "${GREEN}✅你可以输入 ${RED}m${RESET}${GREEN} 或 ${RED}M${RESET}${GREEN} 运行 UU 工具箱${RESET}"
 fi
 
 # ================== 执行脚本 ==================
